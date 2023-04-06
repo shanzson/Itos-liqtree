29a0d9516e45928c57dc0cabbbf1ef6b571873ba
urlaubaitos <austin@itos.fi>
2023-04-06 16:31:39 -0700

    Use a 128.128 number for cummulative interest

--- src/FeeRateSnapshot.sol
+++ src/FeeRateSnapshot.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.18;
 
 struct FeeRateSnapshot {
     uint256 timestamp;
-    uint256 cummulativeInterest; // should this be 256?
+    uint256 cummulativeInterestX128;
 }
 
 
 library FeeRateSnapshotImpl {
 
     function diff(FeeRateSnapshot storage self, FeeRateSnapshot storage other) external view returns (uint256 cummulativeInterestDiff) {
-        cummulativeInterestDiff = self.cummulativeInterest - other.cummulativeInterest;
+        cummulativeInterestDiff = self.cummulativeInterestX128 - other.cummulativeInterestX128;
     }
 
-    function add(FeeRateSnapshot storage self, uint256 interest) external {
-        self.cummulativeInterest += interest;
+    function add(FeeRateSnapshot storage self, uint256 interestX128) external {
+        self.cummulativeInterestX128 += interestX128;
         self.timestamp = block.timestamp;
     }
 
